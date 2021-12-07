c53e59bafd8e08f83d07c7eb25032d8786b397d7
Or Shalmayev <shalmayev1993@gmail.com>
2021-12-07 22:09:25 +0200

    updated

--- Entities/Models/Employee.cs
+++ Entities/Models/Employee.cs
@@ -1,5 +1,12 @@
 public class Employee 
 {
     public Guid Id { get; set; }
+    [Required(ErrorMessage = "Employee name is a required field.")]
+    [MaxLength(50, ErrorMessage = "Employee name cannot be longer than 50 characters.")]
     public string Name { get; set; }
+    [Required(ErrorMessage = "Age is a required field.")]
+    public int Age {get; set;}
+    
+    public string Position { get; set; }
+
 }
